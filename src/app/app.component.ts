75d682ac4a991663567a796c050e96677ddd1ebe
sarojrout <saroj.rout@techblue.co.uk>
2019-04-10 11:20:16 +0530

    formArray#1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formArray';
+  myForm: FormGroup;
+
+  constructor(private _fb: FormBuilder) {
+    this.initiateForm();
+  }
+
+  initiateForm(): void {
+    this.myForm = this._fb.group({
+      productName: '',
+      description: '',
+      specification: this._fb.array([])
+    });
+  }
 }
